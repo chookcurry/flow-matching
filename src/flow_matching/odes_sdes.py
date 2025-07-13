fa42e95bbdfc7a32092aacaf3588799348953140
Max Burzer <maxi.burzer@gmail.com>
2025-07-13 16:24:52 +0200

    restructure

--- src/flow_matching/odes_sdes.py
+++ src/flow_matching/odes_sdes.py
@@ -1,9 +1,9 @@
 from abc import ABC, abstractmethod
 
 import torch
-from tqdm import tqdm
 
 from flow_matching.model import MLPScore, MLPVectorField
+from flow_matching.prob_paths import ConditionalProbabilityPath
 
 
 class ODE(ABC):
@@ -46,6 +46,85 @@ class SDE(ABC):
         pass
 
 
+class ConditionalVectorFieldODE(ODE):
+    def __init__(self, path: ConditionalProbabilityPath, z: torch.Tensor):
+        """
+        Args:
+        - path: the ConditionalProbabilityPath object to which this vector field corresponds
+        - z: the conditioning variable, (1, dim)
+        """
+        super().__init__()
+        self.path = path
+        self.z = z
+
+    def drift_coefficient(self, xt: torch.Tensor, t: torch.Tensor) -> torch.Tensor:
+        """
+        Returns the conditional vector field u_t(x|z)
+        Args:
+            - x: state at time t, shape (bs, dim)
+            - t: time, shape (bs,.)
+        Returns:
+            - u_t(x|z): shape (batch_size, dim)
+        """
+        bs = xt.shape[0]
+        z = self.z.expand(bs, *self.z.shape[1:])
+        return self.path.conditional_vector_field(xt, z, t)
+
+
+class ConditionalVectorFieldSDE(SDE):
+    def __init__(self, path: ConditionalProbabilityPath, z: torch.Tensor, sigma: float):
+        """
+        Args:
+        - path: the ConditionalProbabilityPath object to which this vector field corresponds
+        - z: the conditioning variable, (1, ...)
+        """
+        super().__init__()
+        self.path = path
+        self.z = z
+        self.sigma = sigma
+
+    def drift_coefficient(self, xt: torch.Tensor, t: torch.Tensor) -> torch.Tensor:
+        """
+        Returns the conditional vector field u_t(x|z)
+        Args:
+            - x: state at time t, shape (bs, dim)
+            - t: time, shape (bs,.)
+        Returns:
+            - u_t(x|z): shape (batch_size, dim)
+        """
+        bs = xt.shape[0]
+        z = self.z.expand(bs, *self.z.shape[1:])
+        return self.path.conditional_vector_field(
+            xt, z, t
+        ) + 0.5 * self.sigma**2 * self.path.conditional_score(xt, z, t)
+
+    def diffusion_coefficient(self, xt: torch.Tensor, t: torch.Tensor) -> torch.Tensor:
+        """
+        Args:
+            - x: state at time t, shape (bs, dim)
+            - t: time, shape (bs,.)
+        Returns:
+            - u_t(x|z): shape (batch_size, dim)
+        """
+        return self.sigma * torch.randn_like(xt)
+
+
+class LearnedVectorFieldODE(ODE):
+    def __init__(self, net: MLPVectorField):
+        self.net = net
+
+    def drift_coefficient(self, xt: torch.Tensor, t: torch.Tensor) -> torch.Tensor:
+        """
+        Args:
+            - x: (bs, dim)
+            - t: (bs, dim)
+        Returns:
+            - u_t: (bs, dim)
+        """
+        pred: torch.Tensor = self.net(xt, t)
+        return pred
+
+
 class LangevinFlowSDE(SDE):
     def __init__(self, flow_model: MLPVectorField, score_model: MLPScore, sigma: float):
         """
@@ -80,92 +159,3 @@ class LangevinFlowSDE(SDE):
             - u_t(x|z): shape (batch_size, dim)
         """
         return self.sigma * torch.randn_like(xt)
-
-
-class Simulator(ABC):
-    @abstractmethod
-    def step(self, xt: torch.Tensor, t: torch.Tensor, dt: torch.Tensor) -> torch.Tensor:
-        """
-        Takes one simulation step
-        Args:
-            - xt: state at time t, shape (bs, dim)
-            - t: time, shape (bs,1)
-            - dt: time, shape (bs,1)
-        Returns:
-            - nxt: state at time t + dt (bs, dim)
-        """
-        pass
-
-    @torch.no_grad()
-    def simulate(self, x: torch.Tensor, ts: torch.Tensor) -> torch.Tensor:
-        """
-        Simulates using the discretization gives by ts
-        Args:
-            - x_init: initial state at time ts[0], shape (batch_size, dim)
-            - ts: timesteps, shape (bs, num_timesteps,1)
-        Returns:
-            - x_final: final state at time ts[-1], shape (batch_size, dim)
-        """
-        for t_idx in range(len(ts) - 1):
-            t = ts[:, t_idx]
-            h = ts[:, t_idx + 1] - ts[:, t_idx]
-            x = self.step(x, t, h)
-        return x
-
-    @torch.no_grad()
-    def simulate_with_trajectory(
-        self, x: torch.Tensor, ts: torch.Tensor
-    ) -> torch.Tensor:
-        """
-        Simulates using the discretization gives by ts
-        Args:
-            - x_init: initial state at time ts[0], shape (bs, dim)
-            - ts: timesteps, shape (bs, num_timesteps, 1)
-        Returns:
-            - xs: trajectory of xts over ts, shape (batch_size, num
-            _timesteps, dim)
-        """
-        xs = [x.clone()]
-        nts = ts.shape[1]
-        for t_idx in tqdm(range(nts - 1)):
-            t = ts[:, t_idx]
-            h = ts[:, t_idx + 1] - ts[:, t_idx]
-            x = self.step(x, t, h)
-            xs.append(x.clone())
-        return torch.stack(xs, dim=1)
-
-
-class EulerSimulator(Simulator):
-    def __init__(self, ode: ODE):
-        self.ode = ode
-
-    def step(self, xt: torch.Tensor, t: torch.Tensor, dt: torch.Tensor) -> torch.Tensor:
-        return xt + self.ode.drift_coefficient(xt, t) * dt
-
-
-class EulerMaruyamaSimulator(Simulator):
-    def __init__(self, sde: SDE):
-        self.sde = sde
-
-    def step(self, xt: torch.Tensor, t: torch.Tensor, dt: torch.Tensor) -> torch.Tensor:
-        return (
-            xt
-            + self.sde.drift_coefficient(xt, t) * dt
-            + self.sde.diffusion_coefficient(xt, t)
-            * torch.sqrt(dt)
-            * torch.randn_like(xt)
-        )
-
-
-def record_every(num_timesteps: int, record_every: int) -> torch.Tensor:
-    """
-    Compute the indices to record in the trajectory given a record_every parameter
-    """
-    if record_every == 1:
-        return torch.arange(num_timesteps)
-    return torch.cat(
-        [
-            torch.arange(0, num_timesteps - 1, record_every),
-            torch.tensor([num_timesteps - 1]),
-        ]
-    )
